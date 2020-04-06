779b4a455dfb63232e45160aeb82dddcd5aec042
Zac O <zorndorff@gmail.com>
2020-04-06 10:00:42 -0500

    updates

--- src/lib/data.worker.js
+++ src/lib/data.worker.js
@@ -32,7 +32,7 @@ class DataWorker{
   }
   async loadData() {
     try{
-      const ax =  axios.create({ baseURL: `${window.location.origin}${window.location.pathname}` });
+      const ax =  axios.create({ baseURL: `${self.location.origin}${self.location.pathname}` });
       const {data} = await ax.get('spread_data.json');
       this.data = data;
       this.setState("LOADED");
